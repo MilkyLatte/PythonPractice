970ee2275007027f2a97eaea26f6df3ba8d280cf
MilkyLatte <juanpquinteroo@gmail.com>
2017-12-03 01:06:26 +0000

    Exercise 1

--- Week1/HelloPython.py
+++ Week1/HelloPython.py
@@ -1,1 +1,7 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will
+# turn 100 years old.
+name = input("What is your name?")
+print ("Hello " + name)
 number = int(input("Age?"))
+print("You will be 100 in the year " + str(2017 - number + 100))
